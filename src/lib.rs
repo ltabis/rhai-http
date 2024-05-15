1b732b243f14d6dcae75621779f97f8217f898a5
ltabis <ltabis.pro@gmail.com>
2024-05-15 14:23:01 +0200

    chore: update dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,9 @@ mod rhai_http {
 /// Export the rhai_http module.
 #[no_mangle]
 pub extern "C" fn module_entrypoint() -> rhai::Shared<rhai::Module> {
-
     // The seed must be the same as the one used in the program that will
     // load this module.
-    rhai::config::hashing::set_ahash_seed(Some([1, 2, 3, 4])).unwrap();
+    rhai::config::hashing::set_hashing_seed(Some([1, 2, 3, 4])).unwrap();
 
     rhai::exported_module!(rhai_http).into()
 }
