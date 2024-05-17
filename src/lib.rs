9e4a90f8779df113684dcc1062424e3419beebf4
ltabis <ltabis.pro@gmail.com>
2024-05-17 10:52:50 +0200

    refactor: rename Format parameter to Output, add docs to API, remove dylib entrypoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,22 @@
 use rhai::{def_package, plugin::*};
 
+#[derive(Default, Clone, serde::Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Output {
+    #[default]
+    Text,
+    Json,
+}
+
 #[derive(Clone, serde::Deserialize)]
 pub struct GetParameters {
     url: String,
     #[serde(default)]
     headers: rhai::Map,
     #[serde(default)]
-    body: String,
+    body: rhai::Dynamic,
+    #[serde(default)]
+    output: Output,
 }
 
 #[export_module]
@@ -15,7 +25,18 @@ mod rhai_http {
 
     pub type Client = reqwest::blocking::Client;
 
-    /// Create a new HTTP client.
+    /// Create a new HTTP client. Can be used to query HTTP endpoints.
+    ///
+    /// # Errors
+    ///
+    /// - TLS backend could not be initialized
+    /// - Resolver could not load the system configuration
+    ///
+    /// # Example
+    ///
+    /// ```js
+    /// let client = http::client();
+    /// ```
     ///
     /// # rhai-autodocs:index:1
     #[rhai_fn(return_raw)]
@@ -25,17 +46,46 @@ mod rhai_http {
             .map_err(|error| error.to_string().into())
     }
 
-    // TODO: when does json should be returned ?
     /// Execute a GET request.
+    ///
+    /// # Args
+    ///
+    /// - `parameter`: A map of parameters with the following fields:
+    ///     - `url`: Endpoint to query.
+    ///     - `headers`: Optional headers to add to the query.
+    ///     - `body`: Optional body to add to the query.
+    ///     - `output`: Output format of the response retrieved by the client, can either be 'text' or 'json'. Defaults to 'text'.
+    ///
+    /// # Errors
+    ///
+    /// - The url failed to be parsed
+    /// - Headers failed to be parsed
+    /// - The request failed
+    ///
+    /// # Example
+    ///
+    /// ```js
+    /// let client = http::client();
+    ///
+    /// let response = client.get(#{
+    ///     url: "http://example.com"
+    /// });
+    ///
+    /// print(response)
+    /// ```
     ///
     /// # rhai-autodocs:index:2
     #[rhai_fn(global, pure, return_raw)]
     pub fn get(
         client: &mut Client,
         parameters: rhai::Map,
-    ) -> Result<String, Box<rhai::EvalAltResult>> {
-        let GetParameters { url, headers, body } =
-            rhai::serde::from_dynamic::<GetParameters>(&parameters.into())?;
+    ) -> Result<rhai::Dynamic, Box<rhai::EvalAltResult>> {
+        let GetParameters {
+            url,
+            headers,
+            body,
+            output,
+        } = rhai::serde::from_dynamic::<GetParameters>(&parameters.into())?;
 
         client
             .get(url)
@@ -52,9 +102,12 @@ mod rhai_http {
                     })
                     .collect::<Result<reqwest::header::HeaderMap, Box<EvalAltResult>>>()?,
             )
-            .body(body)
+            .body(body.to_string())
             .send()
-            .and_then(|response| response.text())
+            .and_then(|response| match output {
+                Output::Text => response.text().map(rhai::Dynamic::from),
+                Output::Json => response.json::<rhai::Map>().map(rhai::Dynamic::from),
+            })
             .map_err(|error| error.to_string().into())
     }
 }
@@ -66,21 +119,10 @@ def_package! {
     }
 }
 
-/// Export the module for the dynamic library.
-#[no_mangle]
-pub extern "C" fn module_entrypoint() -> rhai::Shared<rhai::Module> {
-    // The seed must be the same as the one used in the program that will
-    // load this module.
-    rhai::config::hashing::set_hashing_seed(Some([1, 2, 3, 4])).unwrap();
-
-    rhai::exported_module!(rhai_http).into()
-}
-
 #[cfg(test)]
 pub mod test {
-    use rhai::packages::Package;
-
     use crate::HttpPackage;
+    use rhai::packages::Package;
 
     #[test]
     fn simple_query() {
